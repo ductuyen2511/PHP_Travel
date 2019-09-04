70b9c32d55a28569e22a00ae039c8a73b836ebfb
LEON <Leon@DESKTOP-1227H6R>
2019-09-04 21:59:13 +0700

    update

--- testcases/com/phptravel/register/Account_SignUp.java
+++ testcases/com/phptravel/register/Account_SignUp.java
@@ -3,6 +3,7 @@ package com.phptravel.register;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -31,33 +32,33 @@ public class Account_SignUp extends abtractTest {
 	@BeforeClass
 	public void beforeClass(String browserName) {
 		driver = runMultiBrowser(browserName);
-		
+
 		homePage = PageManagementObjects.getHomePageObject(driver);
 		System.out.println("driver testcase layer :" + driver.toString());
 	}
 
+	@BeforeMethod
+	public void runbeforeMethod() {
+		homePage.clickToMyAccountLink();
+		signupPage = homePage.clickToSignUpLink();
+	}
+
 	@Test
 	public void TC_01_InputEmptyAllFields() {
-		log.info("SIGN UP - STEP 01 : Click To My Account Link");
-		homePage.clickToMyAccountLink();
-		
-		log.info("SIGN UP - STEP 02 : Click To Sign Up Link");
-		signupPage = homePage.clickToSignUpLink();
-		
-		log.info("SIGN UP - STEP 03 : Verify Sign up form is displayed");
+		log.info("SIGN UP - STEP 01 : Verify Sign up form is displayed");
 		verifyTrue(signupPage.isSignUpFormDisPlay());
 
-		log.info("SIGN UP - STEP 04 : Click To Sign Up button");
+		log.info("SIGN UP - STEP 02 : Click To Sign Up button");
 		signupPage.clickToSubmitButtonSignUp();
-		
-		log.info("SIGN UP - STEP 05 : Verify empty allfield");
+
+		log.info("SIGN UP - STEP 03 : Verify empty all field");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The Email field is required."), "The Email field is required.");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The Password field is required."), "The Password field is required.");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The Password field is required."), "The Password field is required.");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The First name field is required."), "The First name field is required.");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The Last Name field is required."), "The Last Name field is required.");
 	}
-	
+
 	@Test
 	public void TC_02_InputEmailWithinAt() {
 		log.info("SIGN UP - STEP 01 : Input invalid email");
@@ -67,98 +68,96 @@ public class Account_SignUp extends abtractTest {
 		signupPage.sendkeyToSignUpTextbox("abcabc", "email");
 		signupPage.sendkeyToSignUpTextbox("123456789", "password");
 		signupPage.sendkeyToSignUpTextbox("123456789", "confirmpassword");
-		
+
 		log.info("SIGN UP - STEP 02 : Click to Sign up button");
 		signupPage.clickToSubmitButtonSignUp();
-		
+
 		log.info("SIGN UP - STEP 02 : Verify invalid email message");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The Email field must contain a valid email address."), "The Email field must contain a valid email address.");
 	}
 
 	@Test
 	public void TC_03_InputPasswordLessThanSixDigits() {
-		log.info("SIGN UP - STEP 01 : Clear textbox ");
-		signupPage.ClearToDynamicTextboxSignUpPage("email");
-		signupPage.ClearToDynamicTextboxSignUpPage("password");
-		signupPage.ClearToDynamicTextboxSignUpPage("confirmpassword");
-		
-		log.info("SIGN UP - STEP 02 : Input password less than 6 characters");
-		signupPage.sendkeyToSignUpTextbox(email, "email");
+		log.info("SIGN UP - STEP 01 : Input invalid email");
+		signupPage.sendkeyToSignUpTextbox(firstName, "firstname");
+		signupPage.sendkeyToSignUpTextbox(lastName, "lastname");
+		signupPage.sendkeyToSignUpTextbox("0909181818", "phone");
+		signupPage.sendkeyToSignUpTextbox("abcabc", "email");
 		signupPage.sendkeyToSignUpTextbox("123", "password");
 		signupPage.sendkeyToSignUpTextbox("123", "confirmpassword");
-		
-		log.info("SIGN UP - STEP 03 : Click to Sign up button");
+
+		log.info("SIGN UP - STEP 02 : Click to Sign up button");
 		signupPage.clickToSubmitButtonSignUp();
-		
-		log.info("SIGN UP - STEP 04 : Varify password message");
+
+		log.info("SIGN UP - STEP 03 : Varify password message");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("The Password field must be at least 6 characters in length."), "The Password field must be at least 6 characters in length.");
 	}
-	
+
 	@Test
 	public void TC_04_InputPasswordNotSame() {
-		log.info("SIGN UP - STEP 01 : Clear textbox");
-		signupPage.ClearToDynamicTextboxSignUpPage("password");
-		signupPage.ClearToDynamicTextboxSignUpPage("confirmpassword");
-		
-		log.info("SIGN UP - STEP 02 : Input password and confirm password not same");
-		signupPage.sendkeyToSignUpTextbox("123456789", "password");
-		signupPage.sendkeyToSignUpTextbox("12345", "confirmpassword");
-		
-		log.info("SIGN UP - STEP 03 : Click to Sign up button");
+		log.info("SIGN UP - STEP 01 : Input password and confirm password are not same");
+		signupPage.sendkeyToSignUpTextbox(firstName, "firstname");
+		signupPage.sendkeyToSignUpTextbox(lastName, "lastname");
+		signupPage.sendkeyToSignUpTextbox("0909181818", "phone");
+		signupPage.sendkeyToSignUpTextbox("abcabc", "email");
+		signupPage.sendkeyToSignUpTextbox("123", "password");
+		signupPage.sendkeyToSignUpTextbox("123456789", "confirmpassword");
+
+		log.info("SIGN UP - STEP 02 : Click to Sign up button");
 		signupPage.clickToSubmitButtonSignUp();
-		
-		log.info("SIGN UP - STEP 04 :verify passowrd message");
+
+		log.info("SIGN UP - STEP 03 :verify passowrd message");
 		verifyEquals(signupPage.GetErrorMessageSignUpPage("Password not matching with confirm password."), "Password not matching with confirm password.");
 	}
-	
+
 	@Test
 	public void TC_05_RegisterAccountSuccess() {
-		log.info("SIGN UP - STEP 01 : Clear textbox ");
-		signupPage.ClearToDynamicTextboxSignUpPage("password");
-		signupPage.ClearToDynamicTextboxSignUpPage("confirmpassword");
-		
-		log.info("SIGN UP - STEP 02 : Input password correct all field");
+		log.info("SIGN UP - STEP 01 : Input all valid field");
+		signupPage.sendkeyToSignUpTextbox(firstName, "firstname");
+		signupPage.sendkeyToSignUpTextbox(lastName, "lastname");
+		signupPage.sendkeyToSignUpTextbox("0909181818", "phone");
+		signupPage.sendkeyToSignUpTextbox(email, "email");
 		signupPage.sendkeyToSignUpTextbox("123456789", "password");
 		signupPage.sendkeyToSignUpTextbox("123456789", "confirmpassword");
-		
-		log.info("SIGN UP - STEP 03 : Click to Sign up button");
+
+		log.info("SIGN UP - STEP 02 : Click to Sign up button");
 		accountPage = signupPage.clickToSubmitButtonSignUp();
 
-		log.info("SIGN UP - STEP 04 : verify account page message displayed");
+		log.info("SIGN UP - STEP 03 : verify account page message displayed");
 		verifyEquals(accountPage.isNameCustomerDisplay(), "Hi, " + firstName + " " + lastName);
-		verifyTrue(accountPage.isBookingStatusDisplay("Nothing Booked Yet"));	
-		
-		log.info("SIGN UP - STEP 05 : Click to my profile link");
+		verifyTrue(accountPage.isBookingStatusDisplay("Nothing Booked Yet"));
+
+		log.info("SIGN UP - STEP 04 : Click to my profile link");
 		accountPage.clickToMyProfileLink();
-		
-		log.info("SIGN UP - STEP 06 : Input password");
+
+		log.info("SIGN UP - STEP 05 : Input password");
 		accountPage.inputIntoPasswordTextbox(PASSWORD);
-		
-		log.info("SIGN UP - STEP 07 : Input confirm password");
+
+		log.info("SIGN UP - STEP 06 : Input confirm password");
 		accountPage.inputIntoConfirmPasswordTextbox(CONFIRM_PASSWORD);
-		
-		log.info("SIGN UP - STEP 08 : Click submit button");
+
+		log.info("SIGN UP - STEP 07 : Click submit button");
 		accountPage.clickToSubmitButton();
-		
-		log.info("SIGN UP - STEP 09 : Get email value");
+
+		log.info("SIGN UP - STEP 08 : Get email value");
 		EMAIL = accountPage.getEmailValue("value");
 		log.info("email value" + EMAIL);
-		
-		log.info("SIGN UP - STEP 10 : veirfy message update successfully");
+
+		log.info("SIGN UP - STEP 09 : veirfy message update successfully");
 		verifyTrue(accountPage.isMessageUpdateSuccessfullyDisplayed("Profile Updated Successfully."));
 	}
-	@Test
+
 	public void TC_06_AccountLogout() {
 		log.info("LOG OUT - STEP 01 : click to account dropdown list");
 		accountPage.clickToAccountDropdown();
-		
+
 		log.info("LOG OUT - STEP 02 : click to log out link");
 		loginPage = accountPage.clickToLogoutLink();
-		
+
 		log.info("LOG OUT - STEP 03 : verify login form is displayed");
 		loginPage.isLoginFormDisplayed();
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
